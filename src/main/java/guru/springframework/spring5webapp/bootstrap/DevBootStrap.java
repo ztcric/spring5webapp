4197d618678c1dbe38a275db2ee3a31f8e88a47e
Cliff <tzcliff@outlook.com>
2019-07-23 16:55:48 +0800

    test1

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootStrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootStrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>{
 
     AuthorRepository authorRepository;
     BookRepository bookRepository;
+    PublisherRepository publisherRepository;
 
-    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,16 +30,18 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private void initData(){
 
+        Publisher publisher = new Publisher("J230", "3900");
+        publisherRepository.save(publisher);
 
         Author eric = new Author("Tiancheng", "Zhu");
-        Book ddd = new Book("Book1", "123456", "J230");
+        Book ddd = new Book("Book1", "123456", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
         Author rod = new Author("Cliff", "Zhu");
-        Book noEJB = new Book("Book2", "654321", "J330");
+        Book noEJB = new Book("Book2", "654321", publisher);
         rod.getBooks().add(noEJB);
         authorRepository.save(rod);
         bookRepository.save(noEJB);
